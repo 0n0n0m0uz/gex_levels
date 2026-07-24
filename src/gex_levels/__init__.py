af1504671e0c2e38006b9b400c946f089e8e876e
0n0n0m0uz <40120756+0n0n0m0uz@users.noreply.github.com>
2026-07-23 18:28:53 -0600

    Update __init__.py
    Added info about packages from Python docs

---
+++ src/gex_levels/__init__.py
@@ -0,0 +1,16 @@
+# The __init__.py files are required to make Python treat directories containing 
+# the file as packages (unless using a namespace package, a relatively advanced feature). 
+# This prevents directories with a common name, such as string, from unintentionally 
+# hiding valid modules that occur later on the module search path. 
+
+# In the simplest case, __init__.py can just be an empty file, but it can also execute 
+# initialization code for the package or set the __all__ variable, described later.
+
+# Namespace Packages
+# A subdirectory inside a regular package that does not contain an __init__.py file is 
+# treated as an implicit namespace package (a “namespace subpackage”) rooted in that parent. 
+# See PEP 420 for the underlying specification.
+
+# Basically you can join a bunch of random components located all across wherever and gice the, 
+# the same namespace nd the full package will be held in memory and not actually physically
+# exist in one single location
